25b915b4af804a91e6450554f582714a796fa498
tinnq <tinnq@nustechnology.com>
2018-05-16 17:36:47 +0700

    Update secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,1 @@
-Rails.application.config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>' if ['production'].include? Rails.env
+Deployer::Application.config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>' if ['production'].include? Rails.env
